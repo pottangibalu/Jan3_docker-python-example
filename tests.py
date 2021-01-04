a3c8bebff0b42634482852e5063a42ca0f08b39c
pottangibalu <pottangibalu@gmail.com>
2021-01-03 23:11:57 -0800

    Update tests.py

--- tests.py
+++ tests.py
@@ -5,7 +5,7 @@ class TestHelloWorld(unittest.TestCase):
 
     def test_hello_world(self):
         import app
-        self.assertEqual(app.hello_world(), "Hello, World! -Bala")
+        self.assertEqual(app.hello_world(), "Hello, World!")
 
 if __name__ == '__main__':
     unittest.main()
